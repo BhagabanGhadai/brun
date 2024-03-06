583f4bad1ed34c2244696275e87473c4afbd93e7
Bhagaban Ghadai <102967585+BhagabanGhadai@users.noreply.github.com>
2024-03-07 00:11:58 +0530

    upgraded prism

--- prisma/migrations/20240226041221_upgrade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "Coupon_coupon_code_key";
-
--- DropIndex
-DROP INDEX "Order_razorpay_order_id_key";
--- prisma/migrations/20240306174348_update_product/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "is_gift_for_you" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "is_iconic_essential" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "is_new_arrivals" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "is_top_picks" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "is_trending" BOOLEAN NOT NULL DEFAULT false;
