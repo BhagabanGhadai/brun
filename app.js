8adaa8fc75a1a1c1bbcb48febc2997acdc265b4e
Bhagaban Ghadai <102967585+BhagabanGhadai@users.noreply.github.com>
2024-03-06 15:52:19 +0530

    fixing cors error

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ export const start = (app) => {
     app.use(helmet())
     app.use(express.json({ limit: '1mb' }));
     app.use(express.urlencoded({ extended: true }));
-    app.use(cors({ origin: "*", credentials: true }));
+    app.use(cors());
 
     if (env.NODE_ENV == "dev") app.use(morgan('dev'))
     const limiter = rateLimit({
